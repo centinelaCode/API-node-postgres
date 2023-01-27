ffe1a69bc7152e3261f21915a1198d0766749507
centinela1024tb <centinela1024tb@gmail.com>
2023-01-27 17:10:02 -0600

    [ADD] Configuración basica de un server con express

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('My App')
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+   res.send('My server on express')
+})
+
+app.listen(port, () => {
+   console.log('Server run on port ' + port);
+})
+
