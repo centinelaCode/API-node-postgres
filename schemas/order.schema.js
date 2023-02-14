beb910bef159fdc9ccfda4f6106b2e8544dff5a3
centinela1024tb <centinela1024tb@gmail.com>
2023-02-14 14:57:12 -0600

    [ADD] Se implementa el schema para las orders

---
+++ schemas/order.schema.js
@@ -0,0 +1,16 @@
+const Joi = require('joi');
+
+const id = Joi.number().integer();
+const customerId = Joi.number().integer();
+
+const getOrderSchema = Joi.object({
+   id: id.required(),
+})
+
+const createOrderSchema = Joi.object({
+   customerId: customerId.required(),
+})
+
+
+module.exports = { createOrderSchema, getOrderSchema }
+
